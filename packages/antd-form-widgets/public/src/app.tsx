6ac8d9db82cf4c6695a855bb360573f95850b5a5
afeiship <1290657123@qq.com>
2022-12-13 22:40:01 +0800

    feat: add presefind example

--- packages/antd-form-widgets/public/src/app.tsx
+++ packages/antd-form-widgets/public/src/app.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import AntdFormWidgets from '../../src/main';
-import FormBuilder from '@jswork/antd-form-builder';
+import FormBuilder from '@jswork/antd-form-builder/src/components/form-builder';
 import styled from 'styled-components';
+import { Button, Col, Row } from 'antd';
+import '@jswork/antd-components/src/style.scss';
+import '../../src/main';
 
 const Container = styled.div`
   width: 80%;
@@ -9,15 +11,49 @@ const Container = styled.div`
 `;
 
 export default () => {
+  const items3 = [
+    { value: 's1', label: 'Status1' },
+    { value: 's2', label: 'Status2' },
+    { value: 's3', label: 'Status3' }
+  ];
   const meta = [
     { key: 'login', label: 'LOGIN as' },
-    { key: 'username', label: 'Username' }
+    { key: 'username', label: 'Username' },
+    {
+      key: 'c1',
+      label: 'checkable-dropdown',
+      widget: 'ac:checkable-dropdown',
+      widgetProps: { items: items3 }
+    },
+    {
+      key: 'c2',
+      label: 'Avater',
+      widget: 'ac:upload-picture',
+      widgetProps: {
+        action: 'http://localhost:3200/weibo_api/interface/pic_upload.php'
+      }
+    },
+    {
+      key: 'c3',
+      label: 'Photos',
+      widget: 'ac:upload-picture-card',
+      widgetProps: {
+        action: 'http://localhost:3200/weibo_api/interface/pic_upload.php'
+      }
+    },
+    // {key:'c4', label:"tag-list", widget: 'ac:editable-tag-group'}
   ] as any;
 
   return (
     <Container>
-      <AntdFormWidgets />
-      <FormBuilder meta={meta} />
+      <FormBuilder meta={meta} onFinish={(e) => console.log(e.target.value)}>
+        <Row>
+          <Col span={8}></Col>
+          <Col span={16}>
+            <Button htmlType="submit">Submit</Button>
+          </Col>
+        </Row>
+      </FormBuilder>
     </Container>
   );
 };
--- packages/antd-form-widgets/src/main.ts
+++ packages/antd-form-widgets/src/main.ts
@@ -1,3 +1,14 @@
-import Component from './components';
+import FormBuilder from 'antd-form-builder';
+import {
+  AcCheckableDropdown,
+  AcCheckableTagList,
+  AcUploadPicture,
+  AcEditableTagGroup,
+  AcUploadPictureCard
+} from '@jswork/antd-components/src/main';
 
-export default Component;
+FormBuilder.defineWidget('ac:checkable-dropdown', AcCheckableDropdown);
+FormBuilder.defineWidget('ac:checkable-tag-list', AcCheckableTagList);
+FormBuilder.defineWidget('ac:editable-tag-group', AcEditableTagGroup);
+FormBuilder.defineWidget('ac:upload-picture', AcUploadPicture);
+FormBuilder.defineWidget('ac:upload-picture-card', AcUploadPictureCard);
