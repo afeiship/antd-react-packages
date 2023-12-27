3c59d78f6d1c04c70e7eee1df9a7117ed8f4e8bf
afeiship <1290657123@qq.com>
2023-12-27 17:11:00 +0800

    feat: add input-tags basic code

--- packages/antd-components/src/lib/input-tags.tsx
+++ packages/antd-components/src/lib/input-tags.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import noop from '@jswork/noop';
-import { Input, InputProps, Button } from 'antd';
+import { Input, InputProps, Button, Tag } from 'antd';
 import cx from 'classnames';
 
 // @ https://cdpn.io/iamqamarali/fullpage/qyawoR?anon=true&view=
@@ -10,6 +10,7 @@ type StdCallback = (inEvent: StdEventTarget) => void;
 
 type Props = {
   className?: string;
+  items?: any[];
   onChange?: StdCallback;
 } & InputProps;
 
@@ -17,26 +18,73 @@ export class AcInputTags extends React.Component<Props> {
   static displayName = CLASS_NAME;
   static formSchema = CLASS_NAME;
   static defaultProps = {
+    items: [],
     onChange: noop
+  };
+
+  inputRef = React.createRef<HTMLInputElement>();
+
+  constructor(inProps) {
+    super(inProps);
+    const { items } = inProps;
+    this.state = {
+      items,
+      inputValue: ''
+    };
+  }
+
+  handleInputChange = (inEvent) => {
+    const { value } = inEvent.target;
+    this.setState({ inputValue: value });
+  };
+
+  handleInputKeyAction = (inEvent) => {
+    const { code } = inEvent;
+    const { value } = inEvent.target;
+    const { items } = this.state;
+    const triggerKeys = ['Tab', 'Enter', 'Space'];
+    const val = value.trim();
+    if (code === 'Tab') inEvent.preventDefault();
+    if (triggerKeys.includes(code)) {
+      if (val) {
+        items.push(val);
+        this.setState({ items: items.slice(0), inputValue: '' });
+        this.inputRef.current?.focus();
+      }
+    }
+  };
+
+  handleTagRemove = (inIndex) => {
+    const { items } = this.state;
+    const newItems = items.filter((_, idx) => idx !== inIndex);
+    this.setState({ items: newItems });
   };
 
   render() {
     const { className, ...props } = this.props;
+    const { items, inputValue } = this.state;
 
     return (
-      <div style={{ display: 'flex', gap: '4px', alignItems: 'center', marginBottom: 10 }}>
-        <Button size="small">Tag1</Button>
-        <Button size="small">Tag2</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Button size="small">Tag3</Button>
-        <Input size="small" className={cx(CLASS_NAME, className)} {...props} />
+      <div className={cx(CLASS_NAME, className)}>
+        {items.map((item, idx) => {
+          return (
+            <Tag
+              className={`${CLASS_NAME}__tag`}
+              closeIcon
+              onClose={this.handleTagRemove.bind(this, idx)}
+              key={idx}>
+              {item}
+            </Tag>
+          );
+        })}
+        <input
+          ref={this.inputRef}
+          onInput={this.handleInputChange}
+          onKeyDown={this.handleInputKeyAction}
+          value={inputValue}
+          className={cx(`${CLASS_NAME}__input`, className)}
+          {...props}
+        />
       </div>
     );
   }
