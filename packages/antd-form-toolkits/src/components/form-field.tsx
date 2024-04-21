c677788257341041da84eb7d8a81b91725117add
afeiship <1290657123@qq.com>
2024-04-21 19:53:37 +0800

    feat: use nx classify method

--- packages/antd-form-toolkits/src/components/form-field.tsx
+++ packages/antd-form-toolkits/src/components/form-field.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Form } from 'antd';
 import * as AcComponents from '@jswork/antd-components';
+import nx from '@jswork/next';
+import '@jswork/next-classify';
 
 interface FormFieldProps {
   label: string;
@@ -11,9 +13,6 @@ interface FormFieldProps {
   [key: string]: any;
 }
 
-const camelCase = (str: string) => str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
-const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-
 class FormField extends React.Component<FormFieldProps, any> {
   static defaultProps = {
     widget: 'ac-input'
@@ -21,9 +20,8 @@ class FormField extends React.Component<FormFieldProps, any> {
 
   render() {
     const { label, name, widget, widgetProps, ...restFormItemProps } = this.props;
-    const stdName = capitalize(camelCase(widget!));
-    const Widget = AcComponents[stdName!];
-    console.log('this.props: ', widget, stdName, this.props, AcComponents);
+    const widgetName = nx.classify(widget!);
+    const Widget = AcComponents[widgetName!];
 
     return (
       <Form.Item label={label} name={name} {...restFormItemProps}>
