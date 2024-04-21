5dccef32f09d05001df9e7edcd559876435451b0
afeiship <1290657123@qq.com>
2024-04-22 07:42:12 +0800

    feat: add as props settings

--- packages/antd-form-toolkits/src/components/form-fields.tsx
+++ packages/antd-form-toolkits/src/components/form-fields.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import cx from 'classnames';
 import FormField, { FormFieldProps } from './form-field';
 
 interface FormFieldsProps {
+  className?: string;
   items: FormFieldProps[];
+  as?: string | React.ComponentType<any>;
 }
 
 interface FormFieldsState {}
 
 class FormFields extends React.Component<FormFieldsProps, FormFieldsState> {
   static defaultProps = {
-    items: []
+    items: [],
+    as: React.Fragment
   };
 
   render() {
-    const { items } = this.props;
-    return items.map((field, index) => <FormField key={index} {...field} />);
+    const { className, items, as } = this.props;
+    const AsComponent = as as React.ComponentType<any>;
+    const asProps = as === React.Fragment ? {} : { className: cx('antd-form-fields', className) };
+    const itemsWithIndex = items.map((field, index) => <FormField key={index} {...field} />);
+    return <AsComponent {...asProps} children={itemsWithIndex} />;
   }
 }
 
