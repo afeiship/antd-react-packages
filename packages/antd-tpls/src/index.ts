349ac43850a4cdc3e099e747e11a81bc062d8c8d
afeiship <1290657123@qq.com>
2024-08-18 22:21:12 +0800

    chore: change export modes

--- packages/antd-tpls/src/index.ts
+++ packages/antd-tpls/src/index.ts
@@ -1,4 +1,28 @@
-export * from './lib/breadcrumb';
-export * from './lib/kv';
-export * from './lib/raw';
-export * from './lib/transfer';
+// export * from './lib/breadcrumb';
+// export * from './lib/kv';
+// export * from './lib/raw';
+// export * from './lib/transfer';
+
+import { breadcrumbDefault } from './lib/breadcrumb';
+import { kv, checkboxKv, radioKv, treeKv, selectKv, treeSelectKv } from './lib/kv';
+import { raw, checkboxRaw, radioRaw, selectRaw } from '@/lib/raw';
+import { transferLabel } from './lib/transfer';
+
+export {
+  // breadcrumb
+  breadcrumbDefault,
+  // kv
+  kv,
+  checkboxKv,
+  radioKv,
+  treeKv,
+  selectKv,
+  treeSelectKv,
+  // raw
+  raw,
+  checkboxRaw,
+  radioRaw,
+  selectRaw,
+  // transfer
+  transferLabel,
+};
